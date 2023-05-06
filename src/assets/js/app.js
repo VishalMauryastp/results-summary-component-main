1e26b5240c4e886d0dbaa1cfa3dce9de958ac942
Vishal Maurya <vishalmaurya12nov@gmil.com>
2023-05-06 23:33:43 +0530

    json src update

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,8 +1,7 @@
-$.getJSON("/src/assets/data/data.json", (data) => {
+$.getJSON("./data/data.json", (data) => {
   let totalScore = 0;
   let i = 0;
   $.each(data, (indexInArray, valueOfElement) => {
-    
     $(".rows").append(
       ` <div class="container__summary__rows d-f jc-sb ai-c " style="background-color:${valueOfElement.bgcolor}">
         <div class="logo d-f">
@@ -19,5 +18,5 @@ $.getJSON("/src/assets/data/data.json", (data) => {
     totalScore = totalScore + valueOfElement.score;
   });
   // Math.floor(totalScore/(i*100)*100)
- $(".percent").text( Math.floor(totalScore/(i*100)*100));
+  $(".percent").text(Math.floor((totalScore / (i * 100)) * 100));
 });
